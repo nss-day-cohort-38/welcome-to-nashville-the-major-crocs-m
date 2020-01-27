9340eb17bbc67ed8ae464277ec247114409eb1dd
Bito Mann <bito@bitomann.com>
2020-01-27 14:15:42 -0600

    api data 1

--- src/scripts/concerts-bito/api.js
+++ src/scripts/concerts-bito/api.js
@@ -1,1 +1,20 @@
-const ticketMasterUrl = "https://app.ticketmaster.com/{package}/{version}/{resource}.json?apikey=**{9eIBOsZjX2Jv5L9ko0C7T9ezkmmhs0vH}"
+const ticketMasterBaseUrl = "https://app.ticketmaster.com/discovery/v2/events.json"
+
+// vvv !!DOESN'T WORK YET!!: Takes info user inputs in search field and fetches API data. classificationName= music, stateCode=TN and dmaId=343 (Nashville) vvv //
+
+const searchTicketMaster = (searchInput) => fetch(`${ticketMasterBaseUrl}?apikey=${concertsTicketMasterKey}&stateCode=TN&dmaID=343&classificationName=${searchInput}`)
+.then(tmData => tmData.json())
+.then(searchResults => {
+    console.log(searchResults);
+})
+
+
+const apiManager = {
+    searchTicketMaster(searchCriteria) {
+         // console.log(searchTicketMaster);
+        
+         const url = ticketMasterBaseUrl + `?apikey=${concertsTicketMasterKey}&stateCode=TN&dmaID=343&classificationName=${searchInput}`;
+        
+        return fetch(url).then(resp => resp.json());
+    }
+}
