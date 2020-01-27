aecc9ceb46ffd7c0f0c70f75e922d47bb59f925c
Tyler Simeone <tyler.simeone@gmail.com>
2020-01-27 15:08:58 -0600

    added my script tags

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,1 @@
-// API.getRestaurantData().then(KKDOMEntryManager.KKinputToDom());
-// console.log(API.getRestaurantData())
-const movieSet = API.getRestaurantData();
-// KKDOMEntryManager.KKinputToDom(movieSet)
-console.log(movieSet)
+getArt();
