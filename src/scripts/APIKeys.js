e302e8db695e8b969c83f1eba2faa6461d5658af
Jeremy Mattingly <jamattingly@live.com>
2020-01-27 15:19:04 -0600

    apikeys

--- src/scripts/APIKeys.js
+++ src/scripts/APIKeys.js
@@ -1,10 +1,6 @@
 const APIKeys = {
     "parks": "",
     "restaurants": "d632b0a8019b353f5152265cf01bfb03",
-<<<<<<< HEAD
-    "art": "",    
-=======
     "art": "",       
->>>>>>> master
     "concerts": "9eIBOsZjX2Jv5L9ko0C7T9ezkmmhs0vH"
 }
