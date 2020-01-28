b95757fcc24ee34371d0021c9fac7d637a71f537
Kurt Krafft <kkrafft6609@gmail.com>
2020-01-28 15:22:59 -0600

    added itenerary functionality and then styling changes

--- src/scripts/addeventlistener.js
+++ src/scripts/addeventlistener.js
@@ -8,3 +8,14 @@ buttonKurt.addEventListener('click', KKUserInput.getUserInput);
 // Art button click:
 const artBtn = document.querySelector("#TS-btn");
 artBtn.addEventListener("click", tsAPI.getArtData);
+
+//Save Clickin!
+const resultsDiv = document.getElementById('results-container');
+resultsDiv.addEventListener('click', (event) => {
+    console.log(event);
+    objectCreator.createObjectFromID(event.target.id)
+})
+
+//View Itenerary 
+const viewItenBtn = document.getElementById('see_itenerary');
+viewItenBtn.addEventListener('click', DbAPI.getItenerary);
